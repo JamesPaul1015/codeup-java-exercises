25efc77d7e838a62894eedc61221a0fef6a1c452
jamespaul1015 <jamespaul1015@yahoo.com>
2017-11-16 16:06:50 -0600

    Java Syntax, Types and Variables

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,7 +1,56 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.print("Hello");
-        System.out.print
-                (" World!");
+//        System.out.print("Hello");
+//        System.out.print(" World!");
+//
+//
+//        int myFavoriteNumber = 42;
+//        System.out.println(myFavoriteNumber);
+//
+//
+//
+//        String myString = "To be, or not be, that is the question";
+//        System.out.println(myString);
+//
+//
+//
+//        long myNumber = 123;
+//        System.out.println(myNumber);
+
+
+
+//        int x = 5;
+//        System.out.println(x++);
+//        System.out.println(x);
+
+
+
+//        int x = 5;
+//        System.out.println(++x);
+//        System.out.println(x);
+
+
+//        int x = 4;
+//        x += 5;
+//        System.out.println(x);
+
+
+//        int x = 3;
+//        int y = 4;
+//        y *= x;
+//        System.out.println(y);
+
+
+//        int x = 10;
+//        int y = 2;
+//        x /= y;
+//        y -= x;
+//        System.out.println(x);
+//        System.out.println(y);
+
+
+
+
+
     }
 }
