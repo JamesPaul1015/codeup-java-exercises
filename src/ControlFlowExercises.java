bbec9798e4dc6e6d4e3150a0625750f7aeac5efe
jamespaul1015 <jamespaul1015@yahoo.com>
2017-11-17 15:45:00 -0600

    Console flow Loop Basics-FizzBuzz

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -7,38 +7,38 @@ public class ControlFlowExercises {
 //        }
 
 
-        long x = 2;
-        do {
-            System.out.println(x);
-            x *= x;
-        } while (x < 1000000);
+//        long x = 2;
+//        do {
+//            System.out.println(x);
+//            x *= x;
+//        } while (x < 1000000);
 
 
 
 
 
-//        boolean flag = true;
-//
-//        for(int i=0;i<=100;i++){
-//            if(i%3==0){
-//                System.out.println("Fizz");
-//                flag=false;
-//            }
-//
-//            if(i%5==0){
-//                System.out.println("Buzz");
-//                flag=false;
-//            }
-//
-//            if (i % 15 == 0)
-//            {
-//                System.out.println("FizzBuzz");
-//            }
-//
-//            if (flag)
-//                System.out.println(i);
-//
-//            flag = true; }
+        boolean flag = true;
+
+        for(int i=0;i<=100;i++){
+            if(i%3==0){
+                System.out.println("Fizz");
+                flag=false;
+            }
+
+            if(i%5==0){
+                System.out.println("Buzz");
+                flag=false;
+            }
+
+            if (i % 15 == 0)
+            {
+                System.out.println("FizzBuzz");
+            }
+
+            if (flag)
+                System.out.println(i);
+
+            flag = true; }
 
 
 
