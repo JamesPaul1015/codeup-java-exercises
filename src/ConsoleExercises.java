d7bb2c0db95c4dc10917288e42509b0d5dce8dd9
jamespaul1015 <jamespaul1015@yahoo.com>
2017-11-17 11:59:01 -0600

    Console Input and Output

---
+++ src/ConsoleExercises.java
@@ -0,0 +1,136 @@
+import java.util.Scanner;
+public class ConsoleExercises {
+//    public static void main(String[] args) {
+//        String userInput;
+//        Scanner scan = new Scanner(System.in);
+//
+//        System.out.println("Hello. Please input your name!");
+//        userInput = scan.nextLine();
+//
+//        System.out.println("Hi, " + userInput + "! It's nice to meet you. My name is Hal");
+//        System.out.println("What is your favorite number?");
+//        int userNumber = scan.nextInt();
+//        System.out.println("You told me that your favorite number is " + userNumber);
+
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+//        //Declare Variables
+//        String word1;
+//        String word2;
+//        String word3;
+//
+//        //Accept User Imput
+//        Scanner keyboard=new Scanner(System.in);
+//        System.out.println("Please Enter First Word ");
+//        word1=keyboard.nextLine();
+//        System.out.println("Please Enter Second Word ");
+//        word2=keyboard.nextLine();
+//        System.out.println("Please Enter Third Word ");
+//        word3=keyboard.nextLine();
+//        System.out.println(word1);
+//        System.out.println(word2);
+//        System.out.println(word3);
+
+
+
+
+
+
+//
+//
+//
+//
+//
+//
+//        String userSentence;
+//        System.out.println("Please Enter A Sentence ");
+//        userSentence = scan.nextLine();
+//        System.out.print(userSentence);
+
+//
+//
+//
+//
+//
+//
+//
+//
+//        int width;
+//        int length;
+//
+//
+//        System.out.println("Please enter length of a rectangle");
+//        length = Integer.parseInt(scan.nextLine());
+//
+//     
+//        System.out.println("Please enter width of a rectangle");
+//        width = Integer.parseInt(scan.nextLine());
+//
+//        int perimeter = 2 * (length + width);
+//        int area = length*width;
+//        System.out.println("Area of Rectangle is:"+area);
+//
+//        System.out.println("Perimeter of a rectangle is " + perimeter);
+
+
+
+
+
+
+
+
+
+
+
+
+
+//            public static void main(String[] args) {
+
+//                String s = "Hello World! 3 + 3.0 = 6.0 true ";
+//
+//                // create a new scanner with the specified String Object
+//                Scanner scanner = new Scanner(s);
+//
+//                // find the next int token and print it
+//                // loop for the whole scanner
+//                while (scanner.hasNext()) {
+//
+//                    // if the next is an int, print found and the int
+//                    if (scanner.hasNextInt()) {
+//                        System.out.println("Found :" + scanner.nextInt());
+//                    }
+//                    // if no int is found, print "Not Found:" and the token
+//                    System.out.println("Not Found :" + scanner.next());
+//                }
+
+                // close the scanner
+//                scanner.close();
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,14 +1,14 @@
 public class HelloWorld {
     public static void main(String[] args) {
-//        System.out.print("Hello");
-//        System.out.print(" World!");
-//
-//
-//        int myFavoriteNumber = 42;
-//        System.out.println(myFavoriteNumber);
-//
-//
-//
+        System.out.print("Hello");
+        System.out.println(" World!");
+
+
+        int myFavoriteNumber = 42;
+        System.out.println(myFavoriteNumber);
+
+
+
 //        String myString = "To be, or not be, that is the question";
 //        System.out.println(myString);
 //
@@ -16,31 +16,31 @@ public class HelloWorld {
 //
 //        long myNumber = 123;
 //        System.out.println(myNumber);
-
-
-
+//
+//
+//
 //        int x = 5;
 //        System.out.println(x++);
 //        System.out.println(x);
-
-
-
+//
+//
+//
 //        int x = 5;
 //        System.out.println(++x);
 //        System.out.println(x);
-
-
+//
+//
 //        int x = 4;
 //        x += 5;
 //        System.out.println(x);
-
-
+//
+//
 //        int x = 3;
 //        int y = 4;
 //        y *= x;
 //        System.out.println(y);
-
-
+//
+//
 //        int x = 10;
 //        int y = 2;
 //        x /= y;
@@ -54,3 +54,5 @@ public class HelloWorld {
 
     }
 }
+
+
