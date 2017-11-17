aa0c559af726fbc66de2b4539e32c3f35f58ff8c
jamespaul1015 <jamespaul1015@yahoo.com>
2017-11-17 16:16:48 -0600

    Console flow Loop Basics-Table of Powers

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class ControlFlowExercises {
     public static void main(String[] args) {
 
@@ -17,28 +19,62 @@ public class ControlFlowExercises {
 
 
 
-        boolean flag = true;
+//        boolean flag = true;
+//
+//        for(int i=0;i<=100;i++){
+//            if(i%3==0){
+//                System.out.println("Fizz");
+//                flag=false;
+//            }
+//
+//            if(i%5==0){
+//                System.out.println("Buzz");
+//                flag=false;
+//            }
+//
+//            if (i % 15 == 0)
+//            {
+//                System.out.println("FizzBuzz");
+//            }
+//
+//            if (flag)
+//                System.out.println(i);
+//
+//            flag = true; }
 
-        for(int i=0;i<=100;i++){
-            if(i%3==0){
-                System.out.println("Fizz");
-                flag=false;
-            }
 
-            if(i%5==0){
-                System.out.println("Buzz");
-                flag=false;
-            }
 
-            if (i % 15 == 0)
+
+
+        Scanner sc = new Scanner(System.in);
+        String choice = "y";
+
+        while(choice.equalsIgnoreCase("y"))
+        {
+
+            System.out.println("Enter an Integer: ");
+            int integerNext = sc.nextInt();
+
+            System.out.println("Number" + "  " + "Squared" + "  " + "Cubed");
+            System.out.println("======" + "  " + "======" + "  " + "======");
+
+            for(int i = 1; i <= integerNext; i++)
             {
-                System.out.println("FizzBuzz");
+                i = integerNext;
+                int numberSquared = (int) Math.pow(i, 2);
+                int numberCubed = (int) Math.pow (i, 3);
+
+                String message = "\n" + i + "       " + numberSquared + "       " + numberCubed;
+
+                System.out.println(message);
+                System.out.println();
+
+                // see if the user wants to continue
+                System.out.print("Continue? (y/n): ");
+                choice = sc.next();
+                System.out.println();
             }
-
-            if (flag)
-                System.out.println(i);
-
-            flag = true; }
+        }
 
 
 
