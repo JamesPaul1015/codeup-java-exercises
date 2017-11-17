7ed41620b7747066a50be9830f66d611c23426c2
jamespaul1015 <jamespaul1015@yahoo.com>
2017-11-17 15:17:50 -0600

    Console flow Loop Basics-Do While

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,21 +1,29 @@
 public class ControlFlowExercises {
     public static void main(String[] args) {
 
-        int i = 5;
-        while (i <= 15) {
-            System.out.println(i++);
-        }
+//        int i = 5;
+//        while (i <= 15) {
+//            System.out.println(i++);
+//        }
 
 
-
-
-
-
-
+        long k = 2;
+        do {
+            System.out.println(k);
+            k *= k;
+        } while (k < 1000000);
 
 
     }
-}
+
+    }
+
+
+
+
+
+
+
 
 
 
