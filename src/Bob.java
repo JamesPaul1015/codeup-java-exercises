3d955567373afe7690dd3dd9fa5fc960ef3c0585
jamespaul1015 <jamespaul1015@yahoo.com>
2017-11-20 15:45:33 -0600

    Bob exercise

---
+++ src/Bob.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class Bob {
+    public static void main(String[] args) {
+         {
+            public String hey(String greeting) {
+                greeting = greeting.trim();
+                System.out.println(greeting);
+                if (greeting.isEmpty()) {
+                    return "Fine. Be that way!";
+                } else if (greeting.endsWith() -1 ==!) {
+                    return "Whoa, chill out!";
+                } else if (greeting.charAt(greeting.length() - 1) == '?') {
+                    return "Sure.";
+                } else {
+                    return "Whatever.";
+
+                }
+
+            }
+
+
+        }
+    }
+}
