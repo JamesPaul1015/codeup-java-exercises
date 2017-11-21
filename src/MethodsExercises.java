f6d65ac38a720af76c11d795af4e6f2eec6babc0
jamespaul1015 <jamespaul1015@yahoo.com>
2017-11-21 11:16:25 -0600

    MethodsExercises bonus

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -1,26 +1,153 @@
+import java.util.Random;
 import java.util.Scanner;
 
 public class MethodsExercises {
-    public static void main(String[] args) {
-                Scanner input = new Scanner(System.in);
-                int num1;
-                int num2;
 
-                System.out.print("Enter the first number:");
-                num1 = input.nextInt ();
-                System.out.print("Enter the second number:");
-                num2 = input.nextInt ();
-                System.out.println("Your Math Table is : ");
+    public static void main(String [] args) {
+        Scanner input = new Scanner (System.in);
+        float num1;
+        float num2;
 
-                System.out.println("========================");
-                System.out.println("Addition: " + num1 + " + " + num2 + " = " + (num1 + num2));
-                System.out.println("Subtraction: " + num1 + " - " + num2 + " = " + (num1 - num2));
-                System.out.println("Multiplication: " + num1 + " * " + num2 + " = " + (num1 * num2));
-                System.out.println("Division: " + num1 + " % " + num2 + " = " + (num1 % num2));
+        System.out.print ("Enter the first number:");
+        num1 = input.nextFloat();
+        System.out.print ("Enter the second number:");
+        num2 = input.nextFloat();
+        System.out.println("Your Math Table is : ");
+        System.out.println("========================");
+        System.out.printf ("Addition: %.2f + %.2f = %.2f \n", num1, num2, num1 + num2);
+        System.out.printf ("Subtraction: %.2f - %.2f = %.2f \n", num1, num2, num1 - num2);
+        System.out.printf ("Multiplication: %.4f * %.4f = %.4f \n", num1, num2, num1 * num2);
+        System.out.printf ("Division: %.4f / %.4f = %.4f \n", num1, num2, num1 / num2);
 
 
-            }
-        }
+    }
+}
+
+
+
+
+
+//
+//
+//public class MethodsExercises {
+//
+//    public static void main(String[] args) {
+//        Scanner scanner = new Scanner(System.in);
+//        String choice = "y";
+//
+//        while (choice.equalsIgnoreCase("y") || choice.equalsIgnoreCase("yes")) {
+//
+//            System.out.print("Enter a number between 1 and 31 so that I can find the factorial: ");
+//            int n = scanner.nextInt();
+//            int result = factorial(n);
+//            System.out.println("The factorial of " + n + " is " + result);
+//            System.out.print("Would you like to see another? (y/n): ");
+//            choice = scanner.next();
+//            System.out.println();
+//        }
+//    }
+//    public static int factorial(int n) {
+//        int result = 1;
+//        for (int i = 1; i <= n; i++) {
+//            result = result * i;
+//        }
+//        return result;
+//
+//    }
+//}
+
+
+
+
+
+//
+//
+//public class MethodsExercises {
+//    public static void main(String[] args) {
+//
+//        Random dice = new Random();
+//        Scanner input = new Scanner(System.in);
+//        String choice = "y";
+//
+//        while (choice.equalsIgnoreCase("y") || choice.equalsIgnoreCase("yes")) {
+//            int die1;
+//            int die2;
+//            int roll;
+//            int faces;
+//            int result;
+//            System.out.println("Hey! Dice Roller! How many faces would you like each dice to have?");
+//            System.out.print("Enter any number and press enter to roll the Dice!");
+//            faces = input.nextInt();
+//            result = dice.nextInt(faces) + 1;
+//
+//            die1 = (int) (Math.random() * result) + 1;
+//            die2 = (int) (Math.random() * result) + 1;
+//            roll = die1 + die2;
+//
+//            System.out.println("The first die comes up " + die1);
+//            System.out.println("The second die comes up " + die2);
+//            System.out.println("Your total roll is " + roll);
+//            System.out.print("Would you like to see another? (y/n): ");
+//            choice = input.next();
+//            System.out.println();
+//
+//        }
+//
+//    }
+//}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//import java.util.Scanner;
+//
+//public class MethodsExercises {
+//    public static void main(String[] args) {
+//                Scanner input = new Scanner(System.in);
+//                int num1;
+//                int num2;
+//
+//                System.out.print("Enter the first number:");
+//                num1 = input.nextInt ();
+//                System.out.print("Enter the second number:");
+//                num2 = input.nextInt ();
+//                System.out.println("Your Math Table is : ");
+//
+//                System.out.println("========================");
+//                System.out.println("Addition: " + num1 + " + " + num2 + " = " + (num1 + num2));
+//                System.out.println("Subtraction: " + num1 + " - " + num2 + " = " + (num1 - num2));
+//                System.out.println("Multiplication: " + num1 + " * " + num2 + " = " + (num1 * num2));
+//                System.out.println("Division: " + num1 + " % " + num2 + " = " + (num1 % num2));
+//
+//
+//            }
+//        }
 
 
 
--- src/StringExercise.java
+++ src/StringExercise.java
@@ -1,14 +1,18 @@
-import java.util.Scanner;
 public class StringExercise {
     public static void main(String[] args) {
+        String message;
 
 
+        message = "We don't need no education\n"
+                + "We don't need no thought control\n"
+                + "Check \"this\" out!, \"s inside of \"s!\n"
+                + "In windows, the main drive is usually C:\\\n"
+                + "I can do backslashes \\, double " + "\\" + "\\" + ",\n"
+                + "and the amazing triple backslash \\\\\\!";
 
-
-
-
-
-    }}
+        System.out.println(message);
+    }
+}
 
 
 
