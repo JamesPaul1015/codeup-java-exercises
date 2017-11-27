a49d66e66b131df2df87a7257a33fb5e6a29a6f4
jamespaul1015 <jamespaul1015@yahoo.com>
2017-11-27 12:23:51 -0600

    added BurgerTester and BurgerTools in class exercise

---
+++ src/BurgerTools.java
@@ -0,0 +1,13 @@
+public class BurgerTools {
+}
+//    Create a class of static properties and methods called BurgerTools
+//
+//        Include a string property of mostPopularTopping
+//        Include an int property of averageDaysBeforeExpiration
+//        Include an int property of temperatureWhenCooked
+//        Include a void method called grill() that prints out “Grilling burger”
+//
+//        Create a class called BurgerTester to test assigning and printing property values
+//        and testing methods.
+//
+//        If you have time, add additional properties and methods.
