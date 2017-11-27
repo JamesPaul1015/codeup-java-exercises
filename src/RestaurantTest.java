17634e4c384183723840869bcbb691b2db0dabd5
jamespaul1015 <jamespaul1015@yahoo.com>
2017-11-27 11:51:09 -0600

    just inor changes to Methods Exercises{}

---
+++ src/RestaurantTest.java
@@ -0,0 +1,13 @@
+public class RestaurantTest {
+}
+//    Create a class called “RestaurantDish” (you should not include quotes)
+//
+//        Include an integer property called “costInCents”
+//        Include a string property called “nameOfDish”
+//        Include a boolean property called “wouldRecommend”
+//        Include a method called “eat”
+//        this method will print out “Nom nom nom!”
+//
+
+
+
